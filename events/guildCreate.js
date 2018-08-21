b48c30ac94708df6b01ce78f3eb1bcc1f84e6f76
AustinCreates154 <41809488+AustinCreates154@users.noreply.github.com>
2018-08-21 14:38:22 -0400

    Update guildCreate.js

--- events/guildCreate.js
+++ events/guildCreate.js
@@ -8,8 +8,4 @@ module.exports = (bot, guild) => {
   console.log(`Guild Join\n\nName: ${guild.name}\n\nMembers: ${guild.memberCount}\n\nLarge: ${guild.large}
   
   
-  let em = new RichEmbed()
-  .setTitle("Welcome to Himiachi")
-  .setDescription("Hello! I am Himiachi! Thanks for adding me. Here are a few tips on how to use me.")
-  .addField("Moderation - Let me take care of the baddies. :smug:", "I have a good number of ")
 }
